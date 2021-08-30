57b7e4a43aa31bdca5ed85abdcdd46d77eacb8c7
Lion-alt <lionbee@hispeed.ch>
2021-08-30 08:02:42 +0200

    Initial commit

---
+++ src/main/java/ch/bbw/m226/moviecli/MoviecliApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.m226.moviecli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviecliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviecliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/bbw/m226/moviecli/MoviecliApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.m226.moviecli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviecliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
